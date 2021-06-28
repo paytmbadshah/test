ad898f9fb0a0d298d57a7bcffb1cbbb4e3be18b4
paytmbadshah <email>
2021-06-28 18:21:28 +0530

    Updated Urls

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -26,3 +26,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('ecome.urls'))
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
